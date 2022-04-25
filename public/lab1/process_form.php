7dd3554c40177f852c4f25f4fa0e576f78d12c5a
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-04-25 15:11:49 -0500

    email validate work

--- public/lab1/process_form.php
+++ public/lab1/process_form.php
@@ -1,8 +1,19 @@
 <?php
 
 require __DIR__ . '/config.php';
+require __DIR__ . '/Validator.php';
 
 /* YOUR CODE HERE
 -------------------------------------------- */
 
 $v = new Validator($_POST);
+
+$v->validateEmail('email_address');
+
+if (!empty($v->errors())) {
+
+    $_SESSION['errors'] = $v->errors();
+    $_SESSION['post'] = $v->array();
+    header('Location: form.php');
+    die;
+}
